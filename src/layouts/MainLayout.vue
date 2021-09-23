5455533ba632763d6751e50b1f3b6770327eac2b
Florian Wieland <99wieli@gmail.com>
2021-09-23 14:58:14 +0200

    Project setup

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="text-center">
+          Dough Calculator
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+  
+  setup () {
+    
+
+    return {
+    }
+  }
+})
+</script>
