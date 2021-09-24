1734db59539a8e0dfe8a68d8cd4f7920f0ce2ef3
Florian Wieland <99wieli@gmail.com>
2021-09-24 22:15:21 +0200

    Save expert mode state to localstorage

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,9 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar class="text-black">
-        <q-toolbar-title>
-          Dough Calculator
-        </q-toolbar-title>
+        <q-toolbar-title> Dough Calculator </q-toolbar-title>
         <q-toggle
           v-model="expertMode"
           label="Expert Mode"
@@ -25,6 +23,7 @@
 <script>
 import { defineComponent, ref, watch } from "vue";
 import { useStore } from "vuex";
+import { LocalStorage } from "quasar";
 
 export default defineComponent({
   name: "MainLayout",
@@ -32,9 +31,18 @@ export default defineComponent({
   setup() {
     const store = useStore();
     let expertMode = ref(store.getters.expertMode);
+    if (LocalStorage.getItem("expertMode")) {
+      expertMode.value = true
+      store.dispatch("updateExpertMode", true);
+    } else {
+      expertMode.value = false
+      store.dispatch("updateExpertMode", false);
+    }
+
     watch(expertMode, () => {
       store.dispatch("updateExpertMode", expertMode.value);
-    })
+      LocalStorage.set("expertMode", expertMode.value);
+    });
 
     return {
       expertMode,
